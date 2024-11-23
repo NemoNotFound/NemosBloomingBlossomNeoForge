568d14cd72babb1524e4d33c05f990d66046ec39
NemoNotFound <02grimes-bard@icloud.com>
2024-11-23 13:11:36 +0100

    Refactoring

--- src/main/java/com/nemonotfound/nemosbloomingblossom/world/gen/treedecorator/ModTreeDecoratorType.java
+++ src/main/java/com/nemonotfound/nemosbloomingblossom/world/gen/treedecorator/ModTreeDecoratorType.java
@@ -6,13 +6,13 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
-import static com.mojang.text2speech.Narrator.LOGGER;
 import static com.nemonotfound.nemosbloomingblossom.NemosBloomingBlossom.MOD_ID;
+import static com.nemonotfound.nemosbloomingblossom.NemosBloomingBlossom.LOGGER;
 
 public class ModTreeDecoratorType {
 
     public static final DeferredRegister<TreeDecoratorType<?>> TREE_DECORATOR_TYPES = DeferredRegister.create(Registries.TREE_DECORATOR_TYPE, MOD_ID);
-    public static final DeferredHolder<TreeDecoratorType<?>, TreeDecoratorType<CherryTreeDecorator>> CHERRY_TREE_DECORATOR =  TREE_DECORATOR_TYPES.register("cherry_tree_decorator", () -> new TreeDecoratorType<>(CherryTreeDecorator.CODEC));
+    public static final DeferredHolder<TreeDecoratorType<?>, TreeDecoratorType<CherryTreeDecorator>> CHERRY_TREE_DECORATOR = TREE_DECORATOR_TYPES.register("cherry_tree_decorator", () -> new TreeDecoratorType<>(CherryTreeDecorator.CODEC));
     public static final DeferredHolder<TreeDecoratorType<?>, TreeDecoratorType<FallenLeavesTreeDecorator>> FALLEN_LEAVES_TREE_DECORATOR = TREE_DECORATOR_TYPES.register("fallen_leaves_tree_decorator", () -> new TreeDecoratorType<>(FallenLeavesTreeDecorator.CODEC));
 
     public static void registerTreeDecoratorTypes(IEventBus modEventBus) {
